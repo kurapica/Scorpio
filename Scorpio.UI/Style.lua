5414188c27264cefd26b16777d2db27723a9605e
kurapica <kurapica125@outlook.com>
2019-08-01 00:16:19 +0800

    Fix the style class system

--- Scorpio.UI/Style.lua
+++ Scorpio.UI/Style.lua
@@ -10,19 +10,51 @@ Scorpio           "Scorpio.UI.Style"                 "1.0.0"
 --========================================================--
 
 local clone                     = Toolset.clone
-local ACTION_ADD_CLASS          = 1
+local ACTION_APP_CLASS          = 1
 local ACTION_DEL_CLASS          = 2
 
 local _PropertyOwner
 local _PropertyAccessor
 
 local _Property                 = {}
-local _StyleClass                = {}
-local _FrameStyleClasses        = setmetatable({}, META_WEAKKEY)
+local _StyleClass               = {}
+local _StyleClassObjects        = setmetatable({}, { __index = function(self, key) local val = setmetatable({}, META_WEAKKEY) rawset(self, key, val) return val end })
+local _FrameStyleClasses        = setmetatable({}, META_WEAKALL)
 
 local _ApplyStyleTask           = setmetatable({}, META_WEAKALL)
 local _ApplyStyleTaskStart      = 0
 local _ApplyStyleTaskEnd        = 0
+
+local META_TYPE_PROP_CACHE      = {
+    __index                     = function(self, key)
+        local prop              = _Property[key]
+        if not prop then return nil end
+        local require           = prop.require
+        if require then
+            if getmetatable(require) == nil then
+                local match     = false
+                for _, req in ipairs(require) do
+                    if Class.IsSubType(self[1], req) then match = true break end
+                end
+                if not match then prop = false end
+            else
+                if not Class.IsSubType(self[1], require) then prop = false end
+            end
+        end
+        rawset(self, key, prop)
+        return prop
+    end
+}
+
+local _Property4Type            = setmetatable({}, {
+    __index = function(self, key)
+        if Class.Validate(key) then
+            local cache         = setmetatable({ [1] = key }, META_TYPE_PROP_CACHE)
+            rawset(self, key, cache)
+            return cache
+        end
+    end
+})
 
 function OnEnable(self)
     OnEnable                    = nil
@@ -32,52 +64,117 @@ end
 function ProcessApplyStyleTask()
     while true do
         while _ApplyStyleTaskEnd > _ApplyStyleTaskStart and GetTime() > _ApplyStyleTask[_ApplyStyleTaskStart + 1] do
-            local frame             = _ApplyStyleTask[_ApplyStyleTaskStart + 2]
-            local action            = _ApplyStyleTask[_ApplyStyleTaskStart + 3]
-            local scls              = _ApplyStyleTask[_ApplyStyleTaskStart + 4]
-            local settings          = _StyleClass[scls]
+            local action        = _ApplyStyleTask[_ApplyStyleTaskStart + 2]
+            local frame         = _ApplyStyleTask[_ApplyStyleTaskStart + 3]
+
+            if frame and _ApplyStyleTask[frame] <= _ApplyStyleTaskStart then
+                _ApplyStyleTask[frame] = nil
+            end
 
             _ApplyStyleTask[_ApplyStyleTaskStart + 1] = nil
             _ApplyStyleTask[_ApplyStyleTaskStart + 2] = nil
             _ApplyStyleTask[_ApplyStyleTaskStart + 3] = nil
-            _ApplyStyleTask[_ApplyStyleTaskStart + 4] = nil
-            _ApplyStyleTaskStart    = _ApplyStyleTaskStart + 4
 
-            if settings and frame and not rawget(frame, "Disposed") then
-                -- Apply style on the frame
-                if ACTION_ADD_CLASS == action then
-                    ApplyClassStyle(frame, settings)
-                elseif ACTION_DEL_CLASS == action then
+            if action == ACTION_DEL_CLASS then
+                local settings  = _ApplyStyleTask[_ApplyStyleTaskStart + 4]
+                _ApplyStyleTask[_ApplyStyleTaskStart + 4] = nil
+                _ApplyStyleTaskStart= _ApplyStyleTaskStart + 4
+
+                if frame and not rawget(frame, "Disposed") then
                     RemoveClassStyle(frame, settings)
+                    QueueApplyStyleTask(frame)
                 end
+            else
+                _ApplyStyleTaskStart= _ApplyStyleTaskStart + 3
 
-                Continue()
+                if frame and not rawget(frame, "Disposed") then
+                    ApplyClassStyle(frame, _StyleClass[getmetatable(frame)])
+                    ApplyClassStyle(frame, _StyleClass[_FrameStyleClasses[frame]])
+                end
             end
+
+            Continue()
         end
 
         Next()
     end
 end
 
-function QueueApplyStyleTask(self, action, cls)
+function QueueApplyStyleTask(self)
+    if _ApplyStyleTask[self] then return end
+    _ApplyStyleTask[self]                   = _ApplyStyleTaskEnd
+
     _ApplyStyleTask[_ApplyStyleTaskEnd + 1] = GetTime()
-    _ApplyStyleTask[_ApplyStyleTaskEnd + 2] = self
-    _ApplyStyleTask[_ApplyStyleTaskEnd + 3] = action
-    _ApplyStyleTask[_ApplyStyleTaskEnd + 4] = cls
+    _ApplyStyleTask[_ApplyStyleTaskEnd + 2] = ACTION_APP_CLASS
+    _ApplyStyleTask[_ApplyStyleTaskEnd + 3] = self
+    _ApplyStyleTaskEnd                      = _ApplyStyleTaskEnd + 3
+end
+
+function QueueRemoveStyleTask(self, settings)
+    local previous              = _ApplyStyleTask[self]
+    if previous then
+        if _ApplyStyleTask[previous + 2] == ACTION_APP_CLASS then
+            _ApplyStyleTask[previous + 3]   = false
+        elseif _ApplyStyleTask[_ApplyStyleTaskEnd + 4] == settings then
+            return
+        end
+    end
+
+    _ApplyStyleTask[self]                   = _ApplyStyleTaskEnd
+
+    _ApplyStyleTask[_ApplyStyleTaskEnd + 1] = GetTime()
+    _ApplyStyleTask[_ApplyStyleTaskEnd + 2] = ACTION_DEL_CLASS
+    _ApplyStyleTask[_ApplyStyleTaskEnd + 3] = self
+    _ApplyStyleTask[_ApplyStyleTaskEnd + 4] = settings
     _ApplyStyleTaskEnd                      = _ApplyStyleTaskEnd + 4
 end
 
+function QueueStyleUpdate(scls, old)
+    if old then
+        for frame in pairs(_StyleClassObjects[scls]) do
+            QueueRemoveStyleTask(frame, old)
+        end
+    else
+        for frame in pairs(_StyleClassObjects[scls]) do
+            QueueApplyStyleTask(frame)
+        end
+    end
+end
+
+function GetPropertyCache(self)
+    local cls                   = getmetatable(self)
+    local ptype                 = _Property4Type[cls]
+
+    if not ptype then
+        -- Means the raw bliz ui
+        ptype                   = _Property4Type[UI[self:GetObjectType()]]
+        rawset(_Property4Type, cls, ptype)
+    end
+
+    return ptype
+end
+
 function ApplyClassStyle(self, settings)
-    for prop, v in pairs(settings) do
-        prop.set(self, clone(v, true), 2)
+    if not settings then return end
+    local ptype                 = GetPropertyCache(self)
+    if not ptype then return end
+    for n, v in pairs(settings) do
+        local prop              = ptype[n]
+        if prop then prop.set(self, clone(v, true), 2) end
     end
 end
 
 function RemoveClassStyle(self, settings)
-    for prop in pairs(settings) do
-        if prop.clear   then prop.clear(self, 2) end
-        if prop.default then prop.set(self, clone(prop.default, true), 2) end
-        if prop.nilable then prop.set(self, nil, 2) end
+    if not settings then return end
+    local ptype                 = GetPropertyCache(self)
+    if not ptype then return end
+    for n in pairs(settings) do
+        local prop              = ptype[n]
+        if prop then
+            if prop.clear   then prop.clear(self, 2) end
+            if prop.default then prop.set(self, clone(prop.default, true), 2) end
+            if prop.nilable then prop.set(self, nil, 2) end
+        end
     end
 end
 
@@ -103,7 +200,8 @@ Namespace.SaveNamespace("Scorpio.UI.Style", prototype {
 
         for n, v in pairs(value) do
             if type(n) == "string" then
-                local prop      = _Property[strlower(n)]
+                n               = strlower(n)
+                local prop      = _Property[n]
                 if not prop then error(strformat("Usage: Style[class] = { ... } - the %q isn't a valid property", n), 2) end
 
                 if prop.validate then
@@ -112,7 +210,7 @@ Namespace.SaveNamespace("Scorpio.UI.Style", prototype {
                     v           = ret
                 end
 
-                settings[prop]  = v
+                settings[n]     = v
             end
         end
 
@@ -121,14 +219,10 @@ Namespace.SaveNamespace("Scorpio.UI.Style", prototype {
         if (tkey == "string" and key:match("^[%w_-]+$")) or Class.IsSubType(key, IStyle) then
             -- Style["My_Style"]= { Alpha = 1 }
             key                 = tkey == "string" and strlower(key) or key
-            if _StyleClass[key] then
-                local tprops    = _StyleClass[key]
-                for k, v in pairs(settings) do
-                    tprops[k]   = v
-                end
-            else
-                _StyleClass[key]= settings
-            end
+
+            local old           = _StyleClass[key]
+            _StyleClass[key]    = settings
+            QueueStyleUpdate(key, old)
         else
             error("Usage: Style[class] = { ... } - the style target should be widget class or style class(string)", 2)
         end
@@ -159,52 +253,13 @@ if System.Platform.TYPE_VALIDATION_DISABLED then
         end,
     }
 else
-    local META_TYPE_PROP_CACHE  = {
-        __index                 = function(self, key)
-            local prop          = _Property[key]
-            if not prop then return nil end
-            local require       = prop.require
-            if require then
-                if getmetatable(require) == nil then
-                    local match = false
-                    for _, req in ipairs(require) do
-                        if Class.IsSubType(self[1], req) then match = true break end
-                    end
-                    if not match then return nil end
-                else
-                    if not Class.IsSubType(self[1], require) then return nil end
-                end
-            end
-            rawset(self, key, prop)
-            return prop
-        end
-    }
-
-    local _Property4Type        = setmetatable({}, {
-        __index = function(self, key)
-            if Class.Validate(key) then
-                local cache     = setmetatable({ [1] = key }, META_TYPE_PROP_CACHE)
-                rawset(self, key, cache)
-                return cache
-            end
-        end
-    })
-
     _PropertyAccessor           = prototype {
         __metatable             = Scorpio.UI.Style,
         __index                 = function(self, key)
             if type(key) ~= "string" then error("the widget property name must be string", 2) end
 
-            local cls           = getmetatable(_PropertyOwner)
-            local ptype         = _Property4Type[cls]
-
-            if not ptype then
-                -- Means the raw bliz ui
-                ptype           = _Property4Type[UI[_PropertyOwner:GetObjectType()]]
-                rawset(_Property4Type, cls, ptype)
-            end
-
-            local prop          = ptype[strlower(key)]
+            local ptype         = GetPropertyCache(_PropertyOwner)
+            local prop          = ptype and ptype[strlower(key)]
             if not prop then error(strformat("the object has no widget property named %q", key), 2) end
 
             prop                = prop.get(_PropertyOwner, 2)
@@ -213,16 +268,8 @@ else
         __newindex              = function(self, key, value)
             if type(key) ~= "string" then error("the widget property name must be string", 2) end
 
-            local cls           = getmetatable(_PropertyOwner)
-            local ptype         = _Property4Type[cls]
-
-            if not ptype then
-                -- Means the raw bliz ui
-                ptype           = _Property4Type[UI[_PropertyOwner:GetObjectType()]]
-                rawset(_Property4Type, cls, ptype)
-            end
-
-            local prop          = ptype[strlower(key)]
+            local ptype         = GetPropertyCache(_PropertyOwner)
+            local prop          = ptype and ptype[strlower(key)]
             if not prop then error(strformat("the object has no widget property named %q", key), 2) end
 
             if value == nil then
@@ -360,35 +407,25 @@ end)
 --               Style Class                --
 ----------------------------------------------
 Style.Property  {
-    name    = "Class",
-    type    = NEString,
-    nilable = true,
-    get     = function(self) return _FrameStyleClasses[self] end,
-    set     = function(self, scls)
-        if _FrameStyleClasses[self] == scls then return end
+    name                        = "Class",
+    type                        = NEString,
+    nilable                     = true,
+    get                         = function(self) return _FrameStyleClasses[self] or nil end,
+    set                         = function(self, scls)
+        scls                    = scls and strlower(scls)
 
-        local isStyleCls        = Class.IsObjectType(self, IStyle)
-
-        if _FrameStyleClasses[self] then
-            QueueApplyStyleTask(self, ACTION_DEL_CLASS, _FrameStyleClasses[self])
-
-            if isStyleCls then
-                QueueApplyStyleTask(self, ACTION_ADD_CLASS, getmetatable(self))
-            end
+        local old               = _FrameStyleClasses[self]
+        if old == scls then return end
+        if old then
+            _FrameStyleClasses[self]        = nil
+            _StyleClassObjects[old][self]   = nil
+            QueueRemoveStyleTask(self, _StyleClass[old])
         end
 
         if scls then
-            scls                = strlower(scls)
-
-            if isStyleCls and _FrameStyleClasses[self] == nil then
-                Next(QueueApplyStyleTask, self, ACTION_ADD_CLASS, scls)
-            else
-                QueueApplyStyleTask(self, ACTION_ADD_CLASS, scls)
-            end
-
-            _FrameStyleClasses[self] = scls
-        elseif isStyleCls then
-            _FrameStyleClasses[self] = false
+            _FrameStyleClasses[self]        = scls
+            _StyleClassObjects[scls][self]  = true
+            QueueApplyStyleTask(self)
         end
     end,
 }
@@ -397,9 +434,9 @@ Style.Property  {
 --             Style Interface              --
 ----------------------------------------------
 __Sealed__()
-IStyle                          = interface "Scorpio.UI.Style.IStyle" {
+IStyle                          = interface ("Scorpio.UI.Style.IStyle", {
     function (self)
-        _FrameStyleClasses[self]= _FrameStyleClasses[self] or false
-        QueueApplyStyleTask(self, ACTION_ADD_CLASS, getmetatable(self))
+        _StyleClassObjects[getmetatable(self)][self] = true
+        QueueApplyStyleTask(self)
     end
-}
+})
