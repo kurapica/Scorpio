74ac7948a2dfedf464ea8c13a8a52f56b2bf615d
kurapica <kurapica125@outlook.com>
2022-10-27 00:25:01 +0800

    Update VerticalLayoutManager.lua

--- Modules/Layout/VerticalLayoutManager.lua
+++ Modules/Layout/VerticalLayoutManager.lua
@@ -24,24 +24,50 @@ class "VerticalLayoutManager"   (function(_ENV)
         local prev
         local spacing           = padding and padding.top  or 0
         local showHide          = self.ShowHideChildren
+        local fromTop           = self.BaseLineY ~= JustifyVType.BOTTOM -- From top to bottom
+        local fromLeft          = self.BaseLineX == JustifyHType.LEFT   -- From left to right
+        local fromRight         = self.BaseLineX == JustifyHType.RIGHT  -- From right to left
 
         for i, child, margin in iter do
-            local offsetx       = (padding and padding.left or 0) + (margin and margin.left or 0)
-            local offsety       = spacing + (margin and margin.top  or 0)
+            local offsetx       =  fromLeft  and ( (padding and padding.left  or 0) + (margin and margin.left  or 0) )
+                                or fromRight and ( (padding and padding.right or 0) + (margin and margin.right or 0) )
+                                or ( margin  and ( margin.left and - margin.left or margin.right) or 0  )
+            local offsety       =  fromTop   and ( spacing + (margin and margin.top  or 0) ) or ( spacing + (margin and margin.bottom or 0) )
 
             child:ClearAllPoints()
 
-            if not prev then
-                child:SetPoint("TOP", 0, - offsety)
+            -- Y-axis
+            if fromTop then
+                if not prev then
+                    child:SetPoint("TOP", 0, - offsety)
+                else
+                    child:SetPoint("TOP", prev, "BOTTOM", 0, - offsety)
+                end
             else
-                child:SetPoint("TOP", prev, "BOTTOM", 0, - offsety)
+                if not prev then
+                    child:SetPoint("BOTTOM", 0, offsety)
+                else
+                    child:SetPoint("BOTTOM", prev, "TOP", 0, offsety)
+                end
             end
 
-            child:SetPoint("LEFT", offsetx, 0)
+            -- X-axis
+            if fromLeft then
+                child:SetPoint("LEFT", offsetx, 0)
+            elseif fromRight then
+                child:SetPoint("RIGHT", - offsetx, 0)
+            else
+                child:SetPoint("CENTER", offsetx, 0)
+            end
 
-            if margin and margin.right then
+            -- Stretching
+            if not fromRight and margin and margin.right then
                 child:SetPoint("RIGHT", - ((padding and padding.right or 0) + margin.right), 0)
             end
+            if not fromLeft and margin and margin.left then
+                child:SetPoint("LEFT", ((padding and padding.left or 0) + margin.left), 0)
+            end
+
             if showHide and not child:IsShown() then
                 showHide        = showHide == true and {} or showHide
                 showHide[#showHide + 1] = child
@@ -49,10 +75,10 @@ class "VerticalLayoutManager"   (function(_ENV)
 
             totalHeight         = totalHeight + offsety + child:GetHeight()
             prev                = child
-            spacing             = margin and margin.bottom or 0
+            spacing             = margin and margin[fromTop and "bottom" or "top"] or 0
         end
 
-        totalHeight             = totalHeight + spacing + (padding and padding.bottom or 0)
+        totalHeight             = totalHeight + spacing + (padding and padding[fromTop and "bottom" or "top"] or 0)
         frame:SetHeight(math.max(totalHeight, minHeight))
 
         if type(showHide) == "table" then
@@ -68,12 +94,20 @@ class "VerticalLayoutManager"   (function(_ENV)
     --- Whether show the hidden children when re-layouted
     property "ShowHideChildren" { type = Boolean, default = false }
 
+    --- The base axis of the axis-x
+    property "BaseLineX"        { type = JustifyHType, default = JustifyHType.LEFT }
+
+    --- The base line of the axis-y, MIDDLE is not supported
+    property "BaseLineY"        { type = JustifyVType, default = JustifyVType.TOP }
+
     -----------------------------------------------------------
     --                      constructor                      --
     -----------------------------------------------------------
-    __Arguments__{ Boolean/nil, Boolean/nil }
-    function __ctor(self, includeHideChildren, showHideChildren)
+    __Arguments__{ Boolean/nil, Boolean/nil, JustifyHType/nil, JustifyVType/nil }
+    function __ctor(self, includeHideChildren, showHideChildren, baselineX, baselineY)
         self.IncludeHideChildren= includeHideChildren
         self.ShowHideChildren   = showHideChildren
+        self.BaseLineX          = baselineX
+        self.BaseLineY          = baselineY
     end
 end)
