22e58c64595826b800402f405d31bfbc39290f4b
kurapica <kurapica125@outlook.com>
2024-08-20 21:17:35 +0800

    Update Indicator.lua

--- Modules/Unit/Indicator.lua
+++ Modules/Unit/Indicator.lua
@@ -694,10 +694,23 @@ __Sealed__() class "PredictionHealthBar" (function(_ENV)
         self.overAbsorbGlow     = self:GetChild("OverAbsorbGlow")
         self.overHealAbsorbGlow = self:GetChild("OverHealAbsorbGlow")
 
-        self.totalAbsorb.overlay = self.totalAbsorbOverlay
+        self.totalAbsorb.overlay= self.totalAbsorbOverlay
         self.totalAbsorbOverlay.tileSize = 32
     end
 end)
+
+
+GetTexCoordsForRoleSmallCircle  = _G.GetTexCoordsForRoleSmallCircle or function(role)
+    if ( role == "TANK" ) then
+        return 0, 19/64, 22/64, 41/64
+    elseif ( role == "HEALER" ) then
+        return 20/64, 39/64, 1/64, 20/64
+    elseif ( role == "DAMAGER" ) then
+        return 20/64, 39/64, 22/64, 41/64
+    else
+        error("Unknown role: "..tostring(role))
+    end
+end
 
 ------------------------------------------------------------
 --                     Default Style                      --
