39f122e433442c61feb6d3450a91497e18fa7138
kurapica <kurapica125@outlook.com>
2025-04-20 23:22:40 +0800

    temp save

--- Modules/Unit/Reactive.lua
+++ Modules/Unit/Reactive.lua
@@ -21,10 +21,9 @@ do
     }
 
     --- Gets the unit frame's unit observable
-    function getUnitFrameSubject()
+    function GetUnitFrameSubject()
         -- Gets the current unit frame
         local indicator         = getCurrentTarget()
-        if not (indicator and isUIObject(indicator)) then return end
         while indicator and not isObjectType(indicator, IUnitFrame) do
             indicator           = indicator:GetParent()
         end
@@ -34,13 +33,13 @@ do
     end
 
     --- Combine unit observable and unit event observable
-    function genUnitFrameObservable(unitEvent)
+    function GenUnitFrameObservable(unitEvent)
         local observable        = unitFrameObservable[unitEvent]
 
         if not observable then
             observable          = Observable(function(observer, subscription)
-                local unitSubject = getUnitFrameSubject()
-                if not unitSubject then return unitEvent and unitEvent:Subscribe(observer, subscription) end
+                local unitSub   = GetUnitFrameSubject()
+                if not unitSub  then return unitEvent and unitEvent:Subscribe(observer, subscription) end
 
                 -- Unit event observer
                 local obsEvent  = Observer(function(...) return observer:OnNext(...) end)
@@ -50,7 +49,7 @@ do
                     obsEvent.Subscription = Subscription(subscription)
 
                     -- Check trackable unit
-                    local runit = unit and GetUnitFromGUID(UnitGUID(unit))
+                    local runit = unit and GetUnitFromGUID(UnitGUID(unit)) or unit
                     if runit then unitEvent:MatchUnit(runit):Subscribe(obsEvent) end
 
                     -- Push the unit
@@ -58,7 +57,7 @@ do
                 end)
 
                 -- Start the unit watching
-                unitSubject:Subscribe(obsUnit, subscription)
+                unitSub:Subscribe(obsUnit, subscription)
             end)
 
             unitFrameObservable[unitEvent] = observable
@@ -68,7 +67,7 @@ do
     end
 
     --- Gets the unit owner
-    function getUnitOwner(unit)
+    function GetUnitOwner(unit)
         return unit and (unit:match("^[pP][eE][tT]$") and "player" or unit:gsub("[pP][eE][tT]", "")) or nil
     end
 end
@@ -80,9 +79,9 @@ end
 __Arguments__{ NEString + IObservable, NEString * 0 }
 function Wow.FromUnitEvent(observable, ...)
     if type(observable) == "string" then
-        return genUnitFrameObservable(FromEvent(observable, ...))
+        return GenUnitFrameObservable(FromEvent(observable, ...))
     else
-        return genUnitFrameObservable(observable)
+        return GenUnitFrameObservable(observable)
     end
 end
 
@@ -93,14 +92,14 @@ end
 Scorpio.Wow.Unit                = reactive {
     --- No deep subscription for Unit, override the default subscription
     Subscribe                   = function (self, ...)
-        local subject           = getUnitFrameSubject()
+        local subject           = GetUnitFrameSubject()
         if not subject then return end
         return subject:Subscribe(...)
     end,
 }
 
 ------------------------------------------------------------
---              Scorpio Wow Unit Observable               --
+--                          Unit                          --
 ------------------------------------------------------------
 do
     Unit                        = Scorpio.Wow.Unit
@@ -111,36 +110,15 @@ do
     --- Gets the unit's name
     Unit.Name                   = Unit:Map(GetUnitName)
 
-    --- Gets the unit's owner
-    Unit.Owner                  = Unit:Map(getUnitOwner)
-
     --- Gets the unit's name with server
     Unit.NameWithServer         = Unit:Map(function(unit)
         local name, server      = GetUnitName(unit, true)
         return name and server and (name .. "-" .. server) or name
     end)
 
-    --- Gets the unit's owner name
-    Unit.OwnerName              = Unit:Map(function(unit)
-        local owner             = getUnitOwner(unit)
-        return owner and GetUnitName(owner) or nil
-    end)
-
-    --- Gets the unit's owner name with server
-    Unit.OwnerNameWithServer    = Unit:Map(function(unit)
-        local name, server      = GetUnitName(getUnitOwner(unit), true)
-        return name and server and (name .. "-" .. server) or name
-    end)
-
     --- Gets the unit class color
     Unit.Color                  = Unit:Map(function(unit)
         local _, cls            = UnitClass(unit)
-        return Color[cls or "PALADIN"]
-    end)
-
-    --- Gets the unit's owner class color
-    Unit.OwnerColor             = Unit:Map(function(unit)
-        local _, cls            = UnitClass(getUnitOwner(unit))
         return Color[cls or "PALADIN"]
     end)
 
@@ -208,8 +186,7 @@ do
     end)
 
     --- Gets the unit's disconnected
-    Unit.Disconnected           = Wow.FromUnitEvent("UNIT_HEALTH", "UNIT_CONNECTION"):Next()
-        :Map(function(unit) return not UnitIsConnected(unit) end)
+    Unit.Disconnected           = Wow.FromUnitEvent("UNIT_HEALTH", "UNIT_CONNECTION"):Map(function(unit) return not UnitIsConnected(unit) end)
 
     --- Gets the unit is target
     Unit.IsTarget               = Wow.FromUnitEvent(Wow.FromEvent("PLAYER_TARGET_CHANGED"):Map("=>'any'")):Map(function(unit) return UnitIsUnit(unit, "target") end)
@@ -228,7 +205,7 @@ do
         if UnitHasIncomingResurrection(unit) then
             Next(function()
                 -- Avoid the event not fired when the unit is already resurrected
-                while UnitHasIncomingResurrection(unit) do Delay(1) end
+                while UnitHasIncomigResurrectinon(unit) do Delay(1) end
                 resurrectSubject:OnNext(unit)
             end)
         end
@@ -244,7 +221,7 @@ do
     end)
 
     --- Gets the unit's group roster
-    roleSubject                 = Wow.FromEvent("GROUP_ROSTER_UPDATE", "PLAYER_ROLES_ASSIGNED", "PARTY_LEADER_CHANGED"):Map("=>'any'"):Debounce(0.5)
+    roleSubject                 = Wow.FromEvent("GROUP_ROSTER_UPDATE", "PLAYER_ROLES_ASSIGNED", "PARTY_LEADER_CHANGED"):Map("=>'any'"):Debounce(0.5):ToSubject()
     Unit.GroupRoster            = Wow.FromUnitEvent(roleSubject):Map(function(unit)
         if IsInRaid() and not UnitHasVehicleUI(unit) then
             if GetPartyAssignment('MAINTANK', unit) then
@@ -265,9 +242,6 @@ do
     --- Gets the unit's role
     Unit.Role                   = Wow.FromUnitEvent(roleSubject):Map(UnitGroupRolesAssigned or Toolset.fakefunc)
 
-    --- Gets the unit owner's role
-    Unit.OwnerRole              = Wow.FromUnitEvent(roleSubject):Map(getUnitOwner):Map(UnitGroupRolesAssigned or Toolset.fakefunc)
-
     --- Gets the unit role's visibility
     Unit.RoleVisible            = Unit.Role:Map(function(role) return role and role ~= "NONE" or false end)
 
@@ -276,37 +250,66 @@ do
 
     --- Gets whether the unit is in range of the player
     Unit.InRange                = Unit.Timer:Map(function(unit) return UnitExists(unit) and (UnitIsUnit(unit, "player") or not (UnitInParty(unit) or UnitInRaid(unit)) or UnitInRange(unit)) end)
-
-    --- Gets whether the unit's owner is in range or the player
-    Unit.OwnerInRange           = Unit.Timer:Map(function(unit) unit = getUnitOwner(unit) return UnitExists(unit) and (UnitIsUnit(unit, "player") or not (UnitInParty(unit) or UnitInRaid(unit)) or UnitInRange(getUnitOwner(unit))) end)
 end
 
 ------------------------------------------------------------
---                 Ready Check Observable                 --
+--                       Unit Owner                       --
+------------------------------------------------------------
+do
+    --- Gets the unit's owner
+    Unit.Owner                  = Unit:Map(GetUnitOwner)
+
+    --- Gets the unit's owner name
+    Unit.OwnerName              = Unit:Map(function(unit)
+        local owner             = GetUnitOwner(unit)
+        return owner and GetUnitName(owner) or nil
+    end)
+
+    --- Gets the unit's owner name with server
+    Unit.OwnerNameWithServer    = Unit:Map(function(unit)
+        local name, server      = GetUnitName(GetUnitOwner(unit), true)
+        return name and server and (name .. "-" .. server) or name
+    end)
+
+    --- Gets the unit's owner class color
+    Unit.OwnerColor             = Unit:Map(function(unit)
+        local _, cls            = UnitClass(GetUnitOwner(unit))
+        return Color[cls or "PALADIN"]
+    end)
+
+    --- Gets the unit owner's role
+    Unit.OwnerRole              = Wow.FromUnitEvent(roleSubject):Map(GetUnitOwner):Map(UnitGroupRolesAssigned or Toolset.fakefunc)
+
+    --- Gets whether the unit's owner is in range or the player
+    Unit.OwnerInRange           = Unit.Timer:Map(function(unit) unit = GetUnitOwner(unit) return UnitExists(unit) and (UnitIsUnit(unit, "player") or not (UnitInParty(unit) or UnitInRaid(unit)) or UnitInRange(GetUnitOwner(unit))) end)
+end
+
+------------------------------------------------------------
+--                      Ready Check                       --
 ------------------------------------------------------------
 do
     _ReadyChecking              = 0
-    _ReadyCheckSubject          = Subject()
+    _ReadyCheckVisibleSubject   = Subject()
     _ReadyCheckConfirmSubject   = Subject()
-    _ReadyCheckingCache         = {}
+    _ReadyCheckCache            = {}
 
     __SystemEvent__() __Async__()
     function READY_CHECK()
-        wipe(_ReadyCheckingCache)
+        wipe(_ReadyCheckCache)
 
-        _ReadyChecking              = 1
-        _ReadyCheckSubject:OnNext("any")
+        _ReadyChecking          = 1
+        _ReadyCheckVisibleSubject:OnNext("any")
         _ReadyCheckConfirmSubject:OnNext("any")
 
         if "READY_CHECK_FINISHED" == Wait("READY_CHECK_FINISHED", "PLAYER_REGEN_DISABLED") then
-            _ReadyChecking          = 2
+            _ReadyChecking      = 2
             _ReadyCheckConfirmSubject:OnNext("any")
 
             Wait(10, "PLAYER_REGEN_DISABLED")
         end
 
-        _ReadyChecking              = 0
-        return _ReadyCheckSubject:OnNext("any")
+        _ReadyChecking          = 0
+        return _ReadyCheckVisibleSubject:OnNext("any")
     end
 
     __SystemEvent__()
@@ -315,7 +318,7 @@ do
     end
 
     --- Gets the unit's ready check visibility
-    Unit.ReadyCheckVisible      = Wow.FromUnitEvent(_ReadyCheckSubject):Map(function(unit) return _ReadyChecking > 0 and UnitGUID(unit) ~= nil end)
+    Unit.ReadyCheckVisible      = Wow.FromUnitEvent(_ReadyCheckVisibleSubject):Map(function(unit) return _ReadyChecking > 0 and UnitGUID(unit) ~= nil end)
 
     --- Gets the unit's ready check status
     Unit.ReadyCheck             = Wow.FromUnitEvent(_ReadyCheckConfirmSubject):Map(function(unit)
@@ -324,8 +327,28 @@ do
         local guid              = UnitGUID(unit)
         if not guid then return "notready" end
 
-        local state             = GetReadyCheckStatus(unit) or _ReadyCheckingCache[guid]
-        _ReadyCheckingCache[guid]= state
+        local state             = GetReadyCheckStatus(unit) or _ReadyCheckCache[guid]
+        _ReadyCheckCache[guid]  = state
         return _ReadyChecking == 2 and state == "waiting" and "notready" or state
     end)
+end
+
+------------------------------------------------------------
+--                         Health                         --
+------------------------------------------------------------
+do
+
+end
+
+------------------------------------------------------------
+--                          Power                         --
+------------------------------------------------------------
+do
+
+end
+
+------------------------------------------------------------
+--                          Cast                          --
+------------------------------------------------------------
+do
 end
